60b14c26e77d5e27745765df66d7f6aba11e83f5
Adamtad1216 <nsr.472.14@amu.edu.et>
2025-10-17 14:33:41 +0300

    Updated room routes by adding swagger for testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import roomRoutes from "./routes/roomRoutes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger.js";
 
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/rooms", roomRoutes);
 
 export default app;
