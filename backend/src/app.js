da2ffb2289c3c52340b0ee6bb3e5275a9f6bc2cb
mubarek <mezid58@gmail.com>
2025-10-17 14:23:09 +0300

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
-
+import roomRoutes from "./routes/roomRoutes.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/v1/rooms", roomRoutes);
 
 export default app;
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,15 +2,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
-const MONGO_URI = process.env.MONGO_URI;
+const PORT=process.env.PORT || 8080;
+const MONGO_URI=process.env.MONGO_URI;
 // || "mongodb://localhost:27017/mafia-game"
-const EMAIL_USER = process.env.EMAIL_USER || "your_email@example.com";
-const EMAIL_PASS = process.env.EMAIL_PASS || "your_email_password";
-const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRE || "7d";
-const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
-const ADMIN_PASS = process.env.ADMIN_PASS;
+const EMAIL_USER=process.env.EMAIL_USER || "your_email@example.com";
+const EMAIL_PASS=process.env.EMAIL_PASS || "your_email_password";
+const JWT_SECRET=process.env.JWT_SECRET;
+const JWT_EXPIRES_IN=process.env.JWT_EXPIRE || "7d";
+const ADMIN_EMAIL=process.env.ADMIN_EMAIL;
+const ADMIN_PASS=process.env.ADMIN_PASS;
 
 export default {
   PORT,
